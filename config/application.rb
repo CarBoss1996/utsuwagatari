e130c2daa0b6dfd93dfcc20665df647ef5c1aa86
Kawashita Natsumi <aknatu.bgtn@gmail.com>
2026-04-14 21:30:18 +0900

    ignore lib/generators in autoload_lib for production eager load

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Utuwagatari
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w[assets tasks generators])
 
     config.autoload_paths << Rails.root.join("app/filters")
 
